5a4c41c224b163f0250bdc2b02eab5898401196e
Damian Kajewski <orzechovaty@gmail.com>
2024-09-11 20:19:36 +0200

    update 2024: fixed gradle scripts

--- core/src/com/pupilla/dpk/PlayerController.java
+++ core/src/com/pupilla/dpk/PlayerController.java
@@ -85,6 +85,11 @@ public class PlayerController implements InputProcessor{
     }
 
     @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
@@ -95,7 +100,7 @@ public class PlayerController implements InputProcessor{
     }
 
     @Override
-    public boolean scrolled(int amount) {
+    public boolean scrolled(float amountX, float amountY) {
         return false;
     }
 }
